25d1cabf0660984a4104ace8f9643caac987083e
shem <shem.mahluf@nielsen.com>
2020-06-02 16:59:33 +0300

    first commit

--- app/src/index.js
+++ app/src/index.js
@@ -5,6 +5,14 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (_, res) => {
+    console.log('GET request recieved!')
+    res.status(200).json({
+        type: 'test'
+    })
+})
+
+app.post('/', (_, res) => {
+    console.log('POST request recieved!')
     res.status(200).json({
         type: 'test'
     })
